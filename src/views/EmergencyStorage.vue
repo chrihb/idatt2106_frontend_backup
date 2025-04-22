53fbf9c3b45816f3f0d1f2c654d962351df34cb2
Eskild Smestu <esmestu@icloud.com>
2025-04-22 15:03:55 +0200

    Feat Implement emergency storage view

---
+++ src/views/EmergencyStorage.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import StorageFeed from "@/components/emergencyStorage/StorageFeed.vue";
+</script>
+
+<template>
+  <!-- nav bar -->
+  <h1>Beredskapslager</h1>
+  <StorageFeed/>
+  <!-- item maximized -->
+  <!-- footer -->
+</template>
+
+<style scoped>
+
+</style>
