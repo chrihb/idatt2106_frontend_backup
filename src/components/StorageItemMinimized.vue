0a61737b5be0cbb4199176eda4382d0174eda82f
Eskild Smestu <esmestu@icloud.com>
2025-04-22 14:36:41 +0200

    Feat: Implement storage item minimized

---
+++ src/components/StorageItemMinimized.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps(["name", "amount", "expirationDate", "id"]);
+</script>
+
+<template>
+  <div class="">
+    <p>{{amount}}</p>
+    <p>{{name}}</p>
+    <p>Utløpsdato: {{expirationDate}}</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
