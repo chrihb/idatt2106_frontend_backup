1e46a612609f9d31c4cd8f742996cf66ae9b2eb2
zvindel <erlendez@stud.ntnu.no>
2025-04-23 11:02:01 +0200

    feat: added testing coverage reports for both e2e and unit tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'cypress';
+import codeCoverageTask from '@cypress/code-coverage/task.js'
 
 export default defineConfig({
     e2e: {
-        supportFile: false,
+        setupNodeEvents(on, config) {
+            codeCoverageTask(on, config)
+            return config
+          },
+        supportFile: 'cypress/support/e2e.js',
         specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
         baseUrl: 'http://localhost:5173',
     },
