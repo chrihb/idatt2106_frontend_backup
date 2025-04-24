004c0b2dc8b819782eae81ed7cfb0d8c62a0913e
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-04-24 12:22:25 +0200

    feature custom markers

--- src/stores/mapStore.js
+++ src/stores/mapStore.js
@@ -25,6 +25,14 @@ export const useMapStore = defineStore('mapStore', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(this.map);
+
+            // Add a markers to the map
+            //TODO: Fetch markers from the server
+            ///Example markers
+            this.addMarker(1, 63.446827, 10.421910, "Location 1", "Address 1", "Description 1");
+            this.addMarker(2, 63.446825, 10.431920, "Location 2", "Address 2", "Description 2");
+            this.addMarker(3, 63.446822, 10.441936, "Location 3", "Address 3", "Description 3");
+
         },
         // Start tracking the user's location
         startTracking() {
@@ -52,6 +60,18 @@ export const useMapStore = defineStore('mapStore', {
                 console.error("Geolocation is not supported by your browser.");
             }
         },
+
+        addMarker(id, lat, lng, location, address, description) {
+            const marker = L.marker([lat, lng]).addTo(this.map);
+            marker.bindPopup(`
+                <div class="popup">
+                    <h3>${location}</h3>
+                    <p>${address}</p>
+                    <p>${description}</p>
+                </div>
+            `).openPopup();
+        },
+
         // Set the coordinates of the user's location
         setCoordinates(latitude, longitude) {
             this.latitude = latitude;
