70c76d05caa522a5df5060c7b24250d8f56e1b67
Christian Hess Bore <chrihb@stud.ntnu.no>
2025-04-25 13:25:52 +0200

    Test: added render tests

---
+++ tests/views/AboutUsView.test.js
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect, beforeEach } from 'vitest'
+import i18n from "@/i18n.js"
+import mockRouter from "../mocks/MockRouter.js";
+import mockPinia from "../mocks/MockPinia.js";
+import AboutUsView from "@/views/AboutUsView.vue";
+
+describe('AboutUsView.vue', () => {
+    let wrapper
+
+    beforeEach(async () => {
+        const router = mockRouter();
+
+        wrapper = mount(AboutUsView, {
+            global: {
+                plugins: [i18n, router, mockPinia]
+            }
+        })
+    })
+
+    it('renders correctly', () => {
+        expect(wrapper.isVisible()).toBeTruthy()
+    })
+})
