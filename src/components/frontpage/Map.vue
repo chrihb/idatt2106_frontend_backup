348d420d7a1b425a411f3bc512088fb707eee6e9
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-04-25 10:42:14 +0200

    rebase

--- src/components/frontpage/Map.vue
+++ src/components/frontpage/Map.vue
@@ -1,11 +1,41 @@
-<script setup>
-
-</script>
-
 <template>
-  <p>Map</p>
+  <div id="map" ></div>
+  <div class ="controls">
+    <button @click="centerMapOnUser()">Center on My Location</button>
+    <button @click="toggleTracking">{{ mapStore.canTrack ? 'Stop Tracking' : 'Start Tracking' }}</button>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import {onMounted} from 'vue';
+import 'leaflet/dist/leaflet.css';
+import { useMapStore } from '@/stores/mapStore.js';
 
+const mapStore = useMapStore();
+
+const centerMapOnUser = () => {
+  mapStore.centerMapOnUser();
+};
+
+const toggleTracking = () => {
+  mapStore.toggleTracking();
+};
+
+onMounted(async () => {
+  // Initialize the map
+  try {
+    mapStore.initMap();
+  } catch (error) {
+    console.error('Error initializing map:', error);
+  }
+});
+</script>
+
+<style scoped>
+#map {
+  min-height: 30vh;
+  min-width: 30vh;
+  height: 100%;
+  width: 100%;
+}
 </style>
