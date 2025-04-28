d9bfba9a251e01bca75b1c2eccb47ad277e53498
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-04-28 13:55:19 +0200

    feat created icon tests

--- tests/components/map/Map.test.js
+++ tests/components/map/Map.test.js
@@ -3,6 +3,8 @@ import {describe, it, expect, beforeEach, vi} from 'vitest';
 import Map from '@/components/frontpage/Map.vue';
 import {useMapStore} from '@/stores/mapStore';
 import mockPinia from "../mocks/MockPinia.js";
+import {useMapStore} from '@/stores/mapStore.js';
+import {createCustomMarkerIcon} from "@/utils/markerUtils.js";
 
 
 
@@ -40,6 +42,22 @@ describe('Map.vue', () => {
     it('calls initMap on mounted', () => {
         shallowMount(Map);
         expect(mockMapStore.initMap).toHaveBeenCalled();
+    });
+
+    it('should create a custom marker icon with the correct URL and size', () => {
+        const type = 'Hjertestarter';
+        const icon = createCustomMarkerIcon(type);
+
+        expect(icon.options.iconUrl).toBe('/icons/map/Hjertestarter.png');
+        expect(icon.options.iconSize).toEqual([40, 40]);
+    });
+
+    it('should create a custom marker icon with the correct URL and size', () => {
+        const type = 'Hjertestarter';
+        const icon = createCustomMarkerIcon(type);
+
+        expect(icon.options.iconUrl).toBe('/icons/map/Hjertestarter.png');
+        expect(icon.options.iconSize).toEqual([40, 40]);
     });
 
 })
