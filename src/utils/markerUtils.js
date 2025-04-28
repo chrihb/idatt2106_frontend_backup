080bb38e864dc281ee96aa599af697eb3bb5cf10
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-04-28 12:08:23 +0200

    feat create custom icon

--- src/utils/markerUtils.js
+++ src/utils/markerUtils.js
@@ -7,3 +7,13 @@ export const createMarkerPopup = (type, location,  address, description) =>
                     <p>${description}</p>
                 </div>
     `;
+
+export const createCustomMarkerIcon = (type) => {
+    const iconUrl = `/assets/icons/${type}.png`;
+    return L.icon({
+        iconUrl: iconUrl,
+        iconSize: [25, 41], // size of the icon
+        iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+        popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+    });
+}
