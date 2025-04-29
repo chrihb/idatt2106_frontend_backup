0cde8ef256285069f46101c6bf8de8ec41e925de
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-04-29 11:52:00 +0200

    refactor change name on CenterMapOnUser to CenterMapOnUserButton

--- src/components/map/BasicMap.vue
+++ src/components/map/BasicMap.vue
@@ -1,13 +1,13 @@
 <script setup>
 
 import Map from "@/components/map/Map.vue";
-import CenterMapOnUser from "@/components/map/CenterMapOnUser.vue";
+import CenterMapOnUserButton from "@/components/map/CenterMapOnUserButton.vue";
 </script>
 
 <template>
   <div id="map-container" class="flex-grow relative min-h-140 h-full w-full z-0 rounded-2xl">
     <Map />
-    <CenterMapOnUser />
+    <CenterMapOnUserButton />
   </div>
 </template>
 
