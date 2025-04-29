7b45a9fb38d857c7b8bb4b07e3496ee8538e3020
Eskild Smestu <esmestu@icloud.com>
2025-04-29 13:18:40 +0200

    Fix: Update header based on PO feedback

--- src/components/emergencyStorage/StorageFeed.vue
+++ src/components/emergencyStorage/StorageFeed.vue
@@ -131,7 +131,9 @@ onMounted(async () => {
 
 <template>
   <div class="container mx-auto px-4 py-8">
-    <div class="mb-4 flex justify-end">
+    <div class="mb-4 flex justify-between">
+      <p>warning</p>
+      <h2 class="text-2xl">Categories:</h2>
       <button
           class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200"
           @click="openCreateModal()"
