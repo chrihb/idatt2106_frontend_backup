e5c47e8c850ab3b6209cd497646d73803cdc692c
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-04-30 09:20:58 +0200

    feat created fetchDetailsOfEmergencyZone

--- src/stores/emergencyZoneStore.js
+++ src/stores/emergencyZoneStore.js
@@ -12,10 +12,7 @@ export const emergencyZoneStore = defineStore('emergencyZone', {
             type: '',
             level: 0,
             description: '',
-            geojson: {
-                type: 'Polygon',
-                coordinates: []
-            },
+            coordinates: [],
         }
     }),
     getters: {
@@ -27,49 +24,49 @@ export const emergencyZoneStore = defineStore('emergencyZone', {
         },
     },
     actions: {
-        fetchRestOfEmergencyZone(zoneId) {
-            // Fetch the rest of the emergency zone data from the API
+        fetchDetailsOfEmergencyZone(zoneId) {
+            this.error = null;
+
+            try {
+                const service = emergencyZoneService();
+                //TODO: This is a placeholder for the actual service call
+                const details = service.getEmergencyZoneDetailsMock(zoneId);
+                setRestOfEmergencyZone(details);
+                return details;
+            } catch (error) {
+                console.error('Error fetching emergency zone details:', error);
+                throw error;
+            }
+
+
         },
 
         setBasicEmergencyZone(emergencyZoneData) {
-            this.emergencyZone = {
-                ...this.emergencyZone,
-                zoneId: emergencyZoneData.zoneId,
-                lat: emergencyZoneData.lat,
-                lng: emergencyZoneData.lng,
-                type: emergencyZoneData.type,
-                level: emergencyZoneData.level,
-                geojson: {
-                    type: 'Polygon',
-                    coordinates: emergencyZoneData.geojson.coordinates,
-                },
-            };
+            this.zoneId = emergencyZoneData.zoneId;
+            this.lat = emergencyZoneData.lat;
+            this.lng = emergencyZoneData.lng;
+            this.type = emergencyZoneData.type;
+            this.level = emergencyZoneData.level;
+            this.coordinates = emergencyZoneData.geojson.coordinates;
         },
 
         setRestOfEmergencyZone(emergencyZoneData) {
-            this.emergencyZone = {
-                ...this.emergencyZone,
-                name: emergencyZoneData.name,
-                address: emergencyZoneData.address,
-                description: emergencyZoneData.description,
-            };
+            this.name = emergencyZoneData.name;
+            this.address = emergencyZoneData.address;
+            this.description = emergencyZoneData.description;
         },
 
         setEmergencyZone(emergencyZoneData) {
-            this.emergencyZone = {
-                zoneId: emergencyZoneData.zoneId,
-                name: emergencyZoneData.name,
-                address: emergencyZoneData.address,
-                lat: emergencyZoneData.lat,
-                lng: emergencyZoneData.lng,
-                type: emergencyZoneData.type,
-                level: emergencyZoneData.level,
-                description: emergencyZoneData.description,
-                geojson: {
-                    type: 'Polygon',
-                    coordinates: emergencyZoneData.geojson.coordinates,
-                },
-            };
+            this.zoneId = emergencyZoneData.zoneId;
+            this.name = emergencyZoneData.name;
+            this.address = emergencyZoneData.address;
+            this.lat = emergencyZoneData.lat;
+            this.lng = emergencyZoneData.lng;
+            this.type = emergencyZoneData.type;
+            this.level = emergencyZoneData.level;
+            this.description = emergencyZoneData.description;
+            this.coordinates = emergencyZoneData.coordinates;
+
         },
 
         async saveEmergencyZone() {
@@ -81,20 +78,15 @@ export const emergencyZoneStore = defineStore('emergencyZone', {
         },
 
         clearEmergencyZoneState() {
-            this.emergencyZone = {
-                zoneId: null,
-                name: '',
-                address: '',
-                lat: 0,
-                lng: 0,
-                type: '',
-                level: 0,
-                description: '',
-                geojson: {
-                    type: 'Polygon',
-                    coordinates: []
-                },
-            };
+            this.zoneId = null;
+            this.name = '';
+            this.address = '';
+            this.lat = 0;
+            this.lng = 0;
+            this.type = '';
+            this.level = 0;
+            this.description = '';
+            this.coordinates = [];
         },
     },
     persist: {
