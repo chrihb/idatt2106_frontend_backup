19300ea8cc782efe7f5f7c9bb0fb388b15a8acd5
Sigurd Spook <sigursp@stud.ntnu.no>
2025-05-02 09:25:51 +0200

    fix: test no longer fails

--- tests/components/input/CheckBoxField.test.js
+++ tests/components/input/CheckBoxField.test.js
@@ -1,24 +1,48 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect, beforeEach } from 'vitest'
 import i18n from "@/i18n.js"
-import mockRouter from "../mocks/MockRouter.js";
-import mockPinia from "../mocks/MockPinia.js";
-import CheckboxField from "@/components/input/CheckboxField.vue";
+import { defineRule, configure } from 'vee-validate'
+import * as rules from '@vee-validate/rules'
+import CheckboxField from "@/components/input/CheckboxField.vue"
 
 describe('CheckboxField.vue', () => {
     let wrapper
 
-    beforeEach(async () => {
-        const router = mockRouter();
+    beforeEach(() => {
+        Object.entries(rules).forEach(([ruleName, ruleFn]) => {
+            if (typeof ruleFn === 'function') {
+                defineRule(ruleName, ruleFn);
+            }
+        });
+
+        configure({
+            generateMessage: ctx => `${ctx.field} is invalid`
+        });
 
         wrapper = mount(CheckboxField, {
+            props: {
+                fieldName: 'acceptTerms',
+                label: 'Accept Terms',
+                modelValue: false
+            },
             global: {
-                plugins: [i18n, router, mockPinia]
+                plugins: [i18n]
             }
-        })
-    })
+        });
+    });
 
     it('renders correctly', () => {
         expect(wrapper.isVisible()).toBeTruthy()
-    })
-})
+    });
+
+    it('updates value on toggle', async () => {
+        const checkbox = wrapper.find('input[type="checkbox"]')
+        expect(checkbox.element.checked).toBe(false)
+
+        await checkbox.setValue(true)
+        expect(checkbox.element.checked).toBe(true)
+
+        await checkbox.setValue(false)
+        expect(checkbox.element.checked).toBe(false)
+    });
+});
