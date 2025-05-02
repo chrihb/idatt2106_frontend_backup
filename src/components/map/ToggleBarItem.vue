083f8591cfb21dea589309190f6dbdbe71188391
Ole-Thomas S. Moe <otmoe@stud.ntnu.no>
2025-05-02 15:18:52 +0200

    fix checkbox start as checked

--- src/components/map/ToggleBarItem.vue
+++ src/components/map/ToggleBarItem.vue
@@ -20,6 +20,7 @@ const toggleLayer = (event) => {
     <input
         class="form-checkbox h-5 w-5 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
         type="checkbox"
+        checked
         @change="toggleLayer" />
     {{ layerType }}
   </label>
