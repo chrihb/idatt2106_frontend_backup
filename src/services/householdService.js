05be4dbf004d2c969b92ce963c8f77dc43b02b0b
Christian Hess Bore <chrihb@stud.ntnu.no>
2025-05-06 10:55:54 +0200

    Feat: Create Household

--- src/services/householdService.js
+++ src/services/householdService.js
@@ -19,7 +19,7 @@ export const requestHouseholds = async () => {
         if (response.status !== 200) {
             userStore.clearToken();
             await router.push('/login');
-            return null
+            return []
         }
 
         console.log("Response data:", response.data);
@@ -28,5 +28,70 @@ export const requestHouseholds = async () => {
         console.error('Error checking authentication:', error.code);
         userStore.clearToken();
         await router.push('/login');
+    }
+};
+
+export const joinHousehold = async (inviteCode) => {
+    const userStore = useUserStore();
+
+    try {
+        const response = await axios.post(`${window.backendURL}/api/households/${inviteCode}/join`, {},
+            {
+                headers: {
+                    Authorization: `Bearer ${userStore.token}`
+                }
+            }
+        );
+
+        console.log("Response status:", response.status);
+        if (response.status !== 200) {
+            console.log("Join household failed");
+            return null
+        }
+
+        console.log("Response data:", response.data);
+        const households = await requestHouseholds();
+        console.log("Households after joining:", households);
+        userStore.setCredentials({ householdId: households });
+        return {success: true};
+    } catch (error) {
+        console.error('Error checking authentication:', error.code);
+        return {success: false};
+    }
+};
+
+export const createHousehold = async (name, lat, lon) => {
+    const userStore = useUserStore();
+
+    console.log(name, lat, lon);
+
+    try {
+        const response = await axios.post(`${window.backendURL}/api/households/create`,
+            {
+                name: name,
+                latitude: lat,
+                longitude: lon
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${userStore.token}`
+                }
+            }
+        );
+
+        console.log("Response status:", response.status);
+        if (response.status !== 201) {
+            console.log("Create household failed");
+            return null
+        }
+
+        console.log("Response data:", response.data);
+        const households = await requestHouseholds();
+        console.log("Households after joining:", households);
+        userStore.setCredentials({ householdId: households });
+        return {success: true};
+    } catch (error) {
+        console.error('Error checking authentication:', error.code);
+        return {success: false};
     }
 };
