3c0536e10fe351a1e976752b0c6240002cb5ac90
Eskild Smestu <esmestu@icloud.com>
2025-05-09 18:23:39 +0200

    Fix: hide admin login if user not logged in

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -1,17 +1,15 @@
 <script setup>
-import { useI18n} from "vue-i18n";
+import { useI18n } from "vue-i18n";
+import { useUserStore } from "@/stores/userStore";
 
 const { t } = useI18n();
+const userStore = useUserStore();
 </script>
 
 <template>
   <div class="flex items-center gap-4 h-12 py-1 px-2">
     <RouterLink class="hover:text-kf-white" to="/about-us">{{ t("footer.about-us") }}</RouterLink>
     <RouterLink class="hover:text-kf-white" to="/privacy-policy">{{ t("footer.privacy-policy") }}</RouterLink>
-    <RouterLink class="hover:text-kf-white" to="/admin-login">{{t("login.adminLogin")}}</RouterLink>
+    <RouterLink v-if="userStore.token" class="hover:text-kf-white" to="/admin-login">{{t("login.adminLogin")}}</RouterLink>
   </div>
 </template>
-
-<style scoped>
-
-</style>
